782681669b1270c216fbc2100c3133fd5563b7ac
KrutzVK <krutika95.kk@gmail.com>
2022-02-12 15:33:56 +0300

    Seconf push 12022022

--- js-users/dataHandler.js
+++ js-users/dataHandler.js
@@ -1,24 +1,61 @@
-const url = 'https://assessment-users-backend.herokuapp.com/users.json';
+let dataurl = 'https://assessment-users-backend.herokuapp.com/users.json';
+let userData = ""
+async function getData(url) {
+    let response = await fetch(url);
+    let data = await response.json();
+    return data;
+}
 
-fetch(url).then((response) => {
-    return response.json();
-}).then((data) => {
-    buildTable(data);
-}).catch(function(error) {
-    console.log("error retrieveing data");
-});
+var page = 1
+var rows = 10
 
+function pagination(userData, page, rows) {
 
-function buildTable(data) {
-    var table = document.getElementById('myTable');
+    // console.log(userData);
+    var trimStart = (page - 1) * rows
+    var trimEnd = trimStart + rows
 
-    for (var i = 0; i < data.length; i++) {
-        var row = `<tr>
-            <td>${data[i].first_name}</td>
-            <td>${data[i].last_name}</td>
-            <td>${data[i].created_at}</td>
-                        </tr>`;
-        table.innerHTML += row;
+    var trimmedData = userData.slice(trimStart, trimEnd)
 
+    var pages = Math.ceil(userData.length / rows)
+
+    return {
+        'userData': trimmedData,
+        'pages': pages
     }
 }
+
+function pageButtons(pages) {
+    var wrapper = document.getElementById('wrapper-pagination')
+    wrapper.innerHTML = ''
+
+    for (var page = 1; page <= pages; page++) {
+        wrapper.innerHTML += `<button value=${page} class="page">${page}</button>`
+    }
+
+    const btn = document.querySelector('.page');
+    btn.addEventListener('click', function(event) {
+
+    });
+}
+async function buildTable() {
+    let userData = await getData(dataurl);
+    var table = document.getElementById('myTable');
+
+    var data = pagination(userData, page, rows)
+    console.log(data);
+
+    list = data.userData
+
+    for (var i = 0; i < list.length; i++) {
+        var row = `<tr>
+            <td>${list[i].first_name}</td>
+            <td>${list[i].last_name}</td>
+            <td>${list[i].created_at}</td>
+                        </tr>`;
+        table.innerHTML += row;
+    }
+    pageButtons(data.pages);
+}
+
+buildTable();

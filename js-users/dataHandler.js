feb3e05dffb995fcb775fc624ef5dbc7dbf68ead
KrutzVK <krutika95.kk@gmail.com>
2022-02-14 15:21:57 +0300

    Fourth commit 14022022

--- js-users/dataHandler.js
+++ js-users/dataHandler.js
@@ -4,16 +4,17 @@ let output = ''
 const addUserForm = document.querySelector('.addUserForm')
 const firstNameValue = document.getElementById('firstNameValue')
 const lastNameValue = document.getElementById('lastNameValue')
+const btnSubmit = document.querySelector('.btn');
 
 
 
 const renderUsers = (users) => {
     users.forEach(user => {
-        output += `<tr>
-        <td>${user.first_name}</td>
-        <td>${user.last_name}</td>
-        <td>${user.created_at}</td>
-        <td>${user.status}</td>
+        output += `<tr data-id=${user.id}>
+        <td class="fName">${user.first_name}</td>
+        <td class="lName">${user.last_name}</td>
+        <td class="created">${user.created_at}</td>
+        <td class="status">${user.status}</td>
         <td data-id=${user.id}>
             <a href="#" id="editPost">Edit</a>
             <a href="#" id="deletePost">Delete</a>        
@@ -37,17 +38,48 @@ userList.addEventListener('click', (e) => {
     let id = e.target.parentElement.dataset.id;
     let dataURL = 'https://assessment-users-backend.herokuapp.com/users/'
     let modifyURL = dataURL + id + ".json";
+
     //Delete user
     //Method: DELETE
     if (deletePressed) {
         fetch(modifyURL, {
                 method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
             })
             .then(res => { console.log(res) })
-            .then(() => console.log('done')) //location.reload())
+            .then(() => location.reload())
             .catch(error => console.log(error))
+
     }
-})
+
+    //Edit user
+    //Method: PATCH
+    if (editPressed) {
+        const parent = e.target.parentNode.parentNode;
+        let firstName = parent.querySelector('.fName').textContent;
+        let lastName = parent.querySelector('.lName').textContent;
+
+        firstNameValue.value = firstName;
+        lastNameValue.value = lastName;
+    }
+    btnSubmit.addEventListener('click', (e) => {
+        e.preventDefault();
+        fetch(modifyURL, {
+                method: 'PUT',
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({
+                    first_name: firstNameValue.value,
+                    last_name: lastNameValue.value,
+                    status: "active"
+                })
+            })
+            .then(res => { console.log(res) })
+            .then(() => location.reload())
+            .catch(err => console.log(err))
+    })
+});
 
 //Add new user
 //Method: POST
@@ -72,4 +104,8 @@ addUserForm.addEventListener('submit', (e) => {
             renderUsers(dataArray);
         })
         .catch(err => console.log(err))
+        //reset form
+    firstNameValue.value = '';
+    lastNameValue.value = '';
+
 });

e799903a604b8c2bf076a60ac098f5e1cbd600ea
KrutzVK <krutika95.kk@gmail.com>
2022-02-22 20:38:08 +0300

    Second commit 22022022

--- node-todo/index.js
+++ node-todo/index.js
@@ -1,69 +1,97 @@
-let tbody = document.getElementById('tbody')
-let priorityclass = ""
-let donestatus = ""
-let img = ""
+const Joi = require('joi');
+const express = require('express');
+const app = express();
+app.use(express.json());
 
-
-
-//fetch data
-fetch("http://localhost:3000/todos")
-    .then(res => res.json())
-    .then(json => {
-        json.map(data => {
-            tbody.append(td_fun(data.text, data.done, data.priority));
-        })
-    })
-
-
-
-
-// create data
-function td_fun(text, done, priority) {
-    let td = document.createElement('tr');
-    if (done == true) {
-        donestatus = `<a href="#" onclick="mark()">Mark as done</a>`;
-        img = "correct.png"
-    } else {
-        donestatus = "Done";
-        img = "to-do.png"
+const todos = [{
+        "id": "2",
+        "text": "Edit users on system",
+        "priority": 3,
+        "done": true
+    },
+    {
+        "id": "3",
+        "text": "Delete users from system",
+        "priority": 1,
+        "done": false
+    },
+    {
+        "text": "Post users on system - updated",
+        "priority": 4,
+        "done": true,
+        "id": "fDUHgcU"
     }
-    if (priority <= 2) {
-        priorityclass = "px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800";
-    } else if (priority == 3) {
-        priorityclass = "px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-orange-100 text-orange-800";
-    } else if (priority > 3) {
-        priorityclass = "px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800";
-    }
+];
 
-    td.innerHTML = `
-    <td class="px-6 py-4 whitespace-nowrap">
-        <div class="flex items-center">
-            <div class="flex-shrink-0 h-10 w-10">
-                <img src="${img}" class="h-10 w-10 rounded-full" alt="">
-            </div>
-            <div class="ml-4">
-                <div class="text-sm font-medium text-gray-900">
-                    ${text}
-                </div>
-                <div class="text-sm text-gray-500">
-                    ${donestatus}
-                </div>
-            </div>
-        </div>
-    </td>
-    <td class="px-6 py-4 whitespace-nowrap">
-        <span id="priorityId" class="${priorityclass}">
-            ${priority}
-        </span>
-    </td>
-    <td class="px-6 py-4 whitespace-nowrap">
-        <span class="text-sm text-gray-500">${donestatus}</span>
-    </td>
-    `;
+app.get('/', (req, res) => {
+    res.send('Hello world!!!');
+});
 
-    return td;
+//GET - list of all tasks
+app.get('/api/todos', (req, res) => {
+    res.send(todos);
+});
+
+//GET - find specific task data
+app.get('/api/todos/:id', (req, res) => {
+    //find todo task
+    const todo = todos.find(c => c.id === req.params.id);
+    if (!todo) return res.status(404).send('The todo task with give Id was not found'); //if not found, return 404 object not found
+    res.send(todo); //if found send object
+});
+
+//POST - add new todo
+app.post('/api/todos', (req, res) => {
+    console.log(req.body);
+    // const { error } = validate(req.body); //validate input
+    // if (error) return res.status(400).send(error.details[0].message);
+
+    // //if valid push to array
+    // const todo = {
+    //     id: todos.length + 1,
+    //     text: req.body.text,
+    // };
+    // todos.push(todo);
+    // res.send(todo);
+});
+
+//PUT - update todo
+app.put('/api/todos/:id', (req, res) => {
+    const todo = todos.find(c => c.id === req.params.id); //find task
+    if (!todo) return res.status(404).send('The todo task with give Id was not found'); //if not found, return 404 object not found
+    //validate, if error return 400 - bad request
+
+    const { error } = validate(req.body); //validate input
+    if (error) return res.status(400).send(error.details[0].message); //if invalid input, return 400 bad request
+
+    //if valid input update todos, return updated
+    todo.text = req.body.text;
+    res.send(todo);
+});
+
+//DELETE - remove todo
+app.delete('/api/todos/:id', (req, res) => {
+    const todo = todos.find(c => c.id === req.params.id); //find task
+    if (!todo) return res.status(404).send('The todo task with give Id was not found'); //if not found, return 404 object not found
+
+    //if found remove from array, return removed task
+    const index = todos.indexOf(todo);
+    todos.splice(index, 1);
+    res.send(todo);
+});
+
+//validate input data
+function validate(todo) {
+    const schema = {
+        text: Joi.string().min(3).required(),
+        priority: Joi.number().integer().min(1).max(5),
+        done: Joi.boolean()
+    };
+    const data = Joi.validate(todo, schema);
+    console.log(data);
 }
 
-function mark(e) {
-    console.log(e.target);
-}
+
+//PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
